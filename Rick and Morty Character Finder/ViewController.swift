df678e55fcb16c8b23bc6320bb28142fa87734b0
Murat Baykor <foytingo@gmail>
2020-03-16 13:03:11 +0300

    Initial Commit

---
+++ Rick and Morty Character Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rick and Morty Character Finder
+//
+//  Created by Murat Baykor on 16.03.2020.
+//  Copyright © 2020 Murat Baykor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
